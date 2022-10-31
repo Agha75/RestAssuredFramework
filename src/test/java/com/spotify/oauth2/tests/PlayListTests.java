51233c77a6a35f5a64168850e6e69b8d3ece5800
Agha75 <agharoze.ar@gmail.com>
2022-10-31 21:07:59 +0500

    dfajfnjkasd

--- src/test/java/com/spotify/oauth2/tests/PlayListTests.java
+++ src/test/java/com/spotify/oauth2/tests/PlayListTests.java
@@ -148,6 +148,5 @@ public class PlayListTests extends BaseTest {
     }
     public void testMethod(){
         System.out.println("Method for testing on Jenkins");
-        System.out.println("Method for testing on Jenkins");
     }
 }
